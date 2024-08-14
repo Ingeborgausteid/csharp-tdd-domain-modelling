3d9e3f5a4a472709616c58242d0e4aa204b23064
Ingeborg Brommeland Austeid <ingeborg_austeid@hotmail.com>
2024-08-14 14:06:11 +0200

    adds exercise 2

--- tdd-domain-modelling.CSharp.Main/ShoppingManager.cs
+++ tdd-domain-modelling.CSharp.Main/ShoppingManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,34 @@ namespace tdd_domain_modelling.CSharp.Main
     public class ShoppingManager
     {
 
+        private Dictionary<string, int> _basket = new Dictionary<string, int>();
+
+        public bool Add(string product, int price)
+        {
+            _basket.Add("Banana", 4);
+
+            if (!_basket.ContainsKey(product))
+            {
+                _basket.Add(product, price);
+                return true;
+            }
+            else return false;
+
+        }
+
+
+        public int Total()
+        { 
+            int totalCost = 0;
+
+            foreach (var product in _basket)
+            {
+               totalCost  += product.Value;
+            }
+
+            return totalCost;
+           
+        }
         
     }
 }
--- tdd-domain-modelling.CSharp.Test/ShoppingManagerTests.cs
+++ tdd-domain-modelling.CSharp.Test/ShoppingManagerTests.cs
@@ -6,10 +6,50 @@ namespace tdd_domain_modelling.CSharp.Test
     [TestFixture]
     public class ShoppingManagerTest
     {
+        //As a supermarket shopper, So that I can restock my cupboard, I want to add products into my basket.
         [Test]
-        public void FirstTest()
+        public void AddProductsTest()
         {
-            ShoppingManager core = new ShoppingManager();
+            string product = "Apple";
+            int price = 5;
+            bool expected = true;
+            ShoppingManager shoppingManager = new ShoppingManager();
+
+            bool result = shoppingManager.Add(product, price);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [Test]
+        public void ProductAlreadyInBasketTest()
+        {
+            string product = "Banana";
+            int price = 4;
+            bool expected = false;
+            ShoppingManager shoppingManager = new ShoppingManager();
+
+            bool result = shoppingManager.Add(product, price);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        //As a supermarket shopper, So that I can Pay for products at checkout, I'd like to be able to know the total cost of items in my basket,
+        [Test]
+        public void TotalCostOfBasketTest()
+        {
+
+            string product = "Apple";
+            int price = 5;
+            int expected = 9;
+            ShoppingManager shoppingManager = new ShoppingManager();
+
+            bool addproduct = shoppingManager.Add(product, price);
+            int result = shoppingManager.Total();
+
+            Assert.AreEqual(expected, result);
+
         }
     }
 }
